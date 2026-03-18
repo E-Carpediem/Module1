ea4db0eae5b49631b51f249f814e17a560f24278
zana0422-creator <zana0422@g.eulji.ac.kr>
2026-03-18 22:33:43 +0900

    [FEATURE]강의 등록, 강의 수정  화면 구현#29 모달창 컴포넌트화, 강의 상세 조회 버튼 페이지 이동 수정 추가

--- src/js/components/modalOneButton.js
+++ src/js/components/modalOneButton.js
@@ -1,22 +1,28 @@
-const $btn = document.querySelector(".modal-open-one-btn");
-
-$btn.addEventListener('click', (e) => {
-    document.querySelector('.active-modal').innerHTML = `
-    <div class="modal" data-url="">
+const $btn = document.querySelectorAll(".modal-open-one-btn");
+const defaultMessage = `
+    확인되었습니다.
+`;
+$btn.forEach(($btn) => {
+    $btn.addEventListener('click', (e) => {
+        const moveUrl = $btn.dataset.url;
+        const message = $btn.dataset.message || defaultMessage;
+        document.querySelector('.active-modal').innerHTML = `
+    <div class="modal" data-url="${moveUrl}">
         <div class="modal-container">
             <div class="modal-top">
-                확인되었습니다.
+                ${message}
             </div>
             <div class="modal-bottom modal-check" id="modal-check1">확인</div>
         </div>
     </div>`;
 
-    const $modal = document.querySelector(".modal");
-    $modal.style.display = 'flex';
-    document.querySelectorAll('.modal').forEach((modal) => {
-        modal.querySelector('.modal-check').addEventListener('click', () => movePage(modal));
+        const $modal = document.querySelector(".modal");
+        $modal.style.display = 'flex';
+        document.querySelectorAll('.modal').forEach((modal) => {
+            modal.querySelector('.modal-check').addEventListener('click', () => movePage(modal));
+        });
     });
-})
+});
 
 function movePage(modal) {
     window.location.href = modal.dataset.url;
