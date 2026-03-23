6c796c9f1cd8feea010d0791206c37d3c16c0e33
biyuns <cart0089@naver.com>
2026-03-23 15:52:57 +0900

    [FEATURE] localStorage 더미 데이터 추가#105

---
+++ src/js/manager/managerLectureListTotalCheck.js
@@ -0,0 +1,123 @@
+const contentsListData = JSON.parse(localStorage.getItem('contentsList'));
+
+const $totalContents = document.querySelector(".tmm-lector-content-top-ct");
+const $lectureInf = document.querySelector(".tmm-lecture-inf");
+
+function userTotalManagement(arrayList) {
+    $totalContents.insertAdjacentHTML('afterend',
+        `<div class="tmm-lecture-inf-content">
+            <p> 승인 </p>
+            <p> 강사정보 </p>
+            <p> 강사명 : ${arrayList[6].instructorName} </p>
+            <p> 가입 일자 : 2026.03.14 </p>
+            <p> 전화번호 : 010-1234-5678 </p>
+            <p> 이메일 : honggd@example.com </p>
+        </div>`)
+    
+
+    document.querySelectorAll('.tmm-lector-content').forEach(el => el.remove());
+    const pageMaxArray = arrayList.length < 10 ? arrayList.length : 10;
+    // 페이지네이션 관련 코드입니다. 추후 추가될 예정입니다.
+
+    // const userTotalPagination = Math.ceil(arrayList.length / 10);
+    // const usPaginationCurrentPage = 1;
+    for (let i = pageMaxArray - 1; i >= 0; i--) {
+        $totalContents.insertAdjacentHTML('afterend',
+            `<div class="tmm-lector-content">
+        <p> ${i + 1} </p>
+        <p> ${arrayList[i].contentId} </p>
+        <p> ${arrayList[i].contentTitle} </p>
+        <p> ${arrayList[i].studentNumber}명 </p>
+        <p> ${arrayList[i].lessonsNumber}개 </p>
+        <p> ${arrayList[i].registerDate} </p>
+        <p data-id="${arrayList[i].contentId}"> 삭제하기 </p>
+    </div>`)
+
+    };
+
+    lectureContentsDelete();
+
+}
+
+userTotalManagement(contentsListData);
+const $ArraySignDate = document.querySelector('.m-array>p:nth-of-type(1)');
+const $ArraySort = document.querySelector('.m-array>p:nth-of-type(2)');
+
+$ArraySignDate.addEventListener('click', () => {
+    const ArraySignDate = [...contentsListData].sort((a, b) =>
+        b.contentTitle.localeCompare(a.signDate)
+    );
+    userTotalManagement(ArraySignDate);
+});
+
+$ArraySort.addEventListener('click', () => {
+    const ArraySort = [...contentsListData].sort((a, b) =>
+        b.instructorName.localeCompare(b.userName, 'ko')
+    );
+    userTotalManagement(ArraySort);
+});
+
+
+function lectureContentsDelete() {
+    const $btns = document.querySelectorAll(".tc-content > p:nth-child(8)");
+    const defaultMessage = `
+    삭제하시겠습니까?<br>
+    삭제 후엔 되돌릴 수 없습니다.
+`;
+
+    $btns.forEach(($btn) => {
+        $btn.addEventListener('click', () => {
+            const selectContentId = $btn.dataset.id;
+
+            const moveUrl = $btn.dataset.url;
+            const message = $btn.dataset.message || defaultMessage;
+
+            document.querySelector('.active-modal').innerHTML = `
+        <div class="modal" data-url="${moveUrl}">
+            <div class="modal-container">
+                <div class="modal-top">
+                    ${message}
+                </div>
+                <div class="modal-bottom">
+                    <div class="modal-box modal-close">취소</div>
+                    <div class="modal-box modal-check">확인</div>
+                </div>
+            </div>
+        </div>`;
+
+            const $modal = document.querySelector(".modal");
+            $modal.style.display = 'flex';
+
+            $modal.querySelector('.modal-close').addEventListener('click', () => closeModal($modal));
+
+            $modal.addEventListener('click', (e) => {
+                if (e.target === $modal) closeModal($modal);
+            });
+
+            $modal.querySelector('.modal-check').addEventListener('click', (e) => {
+                console.log(selectContentId);
+                deleteContents(selectContentId);
+                $modal.style.display = 'none';
+            });
+        });
+    });
+
+
+    function closeModal(modal) {
+        modal.style.display = 'none';
+        if (modal.querySelectorAll("form")) {
+            modal.querySelectorAll("form").forEach((form) => {
+                form.reset();
+            });
+        }
+    }
+}
+
+function deleteContents() {
+    const targetIndex = contentsListData.findIndex(item => {
+        return item.contentId;
+    });
+    let a = contentsListData.splice(targetIndex, 1);
+    localStorage.setItem('contentsList', JSON.stringify(contentsListData));
+    userTotalManagement(contentsListData);
+}
