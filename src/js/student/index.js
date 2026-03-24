ae5f3f12b05898740c9d5f6884cdf62ad415e2c8
zana0422-creator <zana0422@g.eulji.ac.kr>
2026-03-24 14:41:03 +0900

    [STYLE] 강의 상세 조회 코드 소량 수정 #158

--- src/js/student/index.js
+++ src/js/student/index.js
@@ -41,6 +41,9 @@ function escapeHTML(value) {
 // 강의 렌더링
 function renderLectures() {
     const lectureList = getLectureList();
+    const lectureListBox = $('.ct-lecture-list');
+
+    lectureListBox.innerHTML = "";
 
     lectureList.forEach(item => {
         const card = document.createElement('div');
